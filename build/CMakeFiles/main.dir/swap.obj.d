e94213b5f4a68598390439280f3668ac355852e9
tcy <1908425256@qq.com>
2022-03-16 15:20:39 +0800

    master

---
+++ build/CMakeFiles/main.dir/swap.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/swap.obj: \
+ C:\Users\Administrator\Desktop\TEST3\swap.cpp \
+ C:\Users\Administrator\Desktop\TEST3\swap.h
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"swap.h"
+
+using namespace std;
+
+int main()
+{
+    int a = 1725, b  = 20;
+    cout<<a<<":weer3:"<<b<<endl;
+    swap(a, b);
+    cout<<a<<":ffassg:"<<b<<endl;
+
+    return 0;
+}
